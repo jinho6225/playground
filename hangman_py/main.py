6a7606106bf9753dfe0b0bfd63e73d6552118086
jinho6225 <jinho6225@hotmail.com>
2021-01-18 13:18:21 -0800

    added edge case, err handle

--- hangman_py/main.py
+++ hangman_py/main.py
@@ -1,7 +1,9 @@
-import random, math
+import random, math, re
 
 def hangman():
+	print('welcome to hangman game!\nanswer would be one of the biggest tech company\ngood luck!')
 	answer = ['apple', 'microsoft', 'facebook', 'netflix', 'amazon', 'uber', 'lyft', 'twitter', 'google']
+	store = []
 	num = math.floor(random.random() * len(answer))
 	goal = answer[num]
 	chance = len(goal)
@@ -9,6 +11,19 @@ def hangman():
 	
 	while chance > 0:
 		letter = input('please enter one of alphabet between \"a\" and \"z\": ' )
+		if letter.isalpha():
+			if letter in store:
+				idx = store.index(letter)
+				letter = input(f'you already used \'{store[idx]}\'\nplease enter another alphabet: ' )
+				if letter in store:
+					idx = store.index(letter)
+					letter = print(f'you already used \'{store[idx]}\'')
+					continue;
+			else:
+				if letter in goal:
+					store.append(letter)
+		else:
+			letter = input('please enter alphabet only between \"a\" and \"z\": ' )
 		print(f'\nyou left {chance-1} chance')
 		if not goal.find(letter.lower()) == -1:
 			idx = goal.find(letter.lower())
@@ -21,15 +36,15 @@ def hangman():
 			chance -= 1
 		
 		if string in answer:
-			answer = input('congraturation!!\nstart again?\nplease enter \'yes\' or \'no\' ')
-			if answer == 'yes':
+			answer = input('congraturation!!\nstart again?\nplease enter \'yes\' or \'y\', otherwise enter anykey to exit here ')
+			if answer.lower() == 'yes' and answer.lower() == 'y':
 				return hangman()
 			else:
 				print('bye bye')
 				return
 
 	if chance == 0:
-		answer = input('lost game\nstart again?\nplease enter \'yes\' or \'no\' ')
+		answer = input('lost game\nstart again?\nplease enter \'yes\' or \'y\', otherwise enter anykey to exit here ')
 		if answer == 'yes':
 			return hangman()
 		else:

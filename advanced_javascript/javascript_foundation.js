8c632a03c190d6da00b17eaf78c9da97bc8a87a6
jinho6225 <jinho6225@hotmail.com>
2021-02-03 10:54:06 -0800

    javascript fundation

---
+++ advanced_javascript/javascript_foundation.js
@@ -0,0 +1,26 @@
+/**
+ * JavaScript Engine
+ *
+ * javascript file -> javascript engine -> computer
+ * javascript engine read javascript file and tell to computer to do something based on javascript file
+ *
+ * V8, SpiderMonkey, Chakra, etc...
+ * V8 by google (in order to make browser to perform better(fast))
+ *
+ * who created the first Javascript Engine
+ * Brendan Eich - early version spider monkey
+ *
+ * V8 - chrome, node.js - it's famouse engine
+ *
+ * javascript file -> parse -> AST -> interpreter -> profiler -> compiler -> optimized code -> 101010100011100 like that
+ *                                      ->  bytecode
+ * call stack / memory heap
+ *
+ * List of ECMAScript engines - wikipedia
+ *
+ */
+
+/**
+ * interpreter and compiler
+ *
+ */

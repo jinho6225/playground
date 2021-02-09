3177c7572b0e146eb82632044c7a511c4c8357d9
jinho6225 <jinho6225@hotmail.com>
2021-02-08 20:04:08 -0800

    this.timeout, this.retries, skip, only, xdescribe

--- mocha-demo/test/async.app.spec.js
+++ mocha-demo/test/async.app.spec.js
@@ -2,29 +2,39 @@ const expect = require("chai").expect;
 const axios = require("axios");
 
 // BDD
-describe("async test suite", () => {
-  //   it("promised based way", () => {
-  //     return axios.get("https://reqres.in/api/users/2").then((res) => {
-  //       expect(res.data.data.email).to.be.equal("janet.weaver@reqres.int");
-  //     });
-  //   });
+describe("async test suite", function () {
+  //pending stats
+  //xdescribe("async test suite", function () {
+  //describe.skip("async test suite", function () {
 
-  it("Test 2", () => {
+  //   this.retries(1);
+
+  it("promised based way", function () {
+    //  it.only("promised based way", function () {
+    // this.retries(2);
+    this.timeout(2000);
+    return axios.get("https://reqres.in/api/users/2").then((res) => {
+      expect(res.data.data.email).to.be.equal("janet.weaver@reqres.in");
+    });
+  });
+
+  it("Test 2", function () {
     console.log("test2");
   });
-  //   it("done based way", (done) => {
-  //     axios
-  //       .get("https://reqres.in/api/users/2")
-  //       .then((res) => {
-  //         expect(res.data.data.email).to.be.equal("janet.weaver@reqres.in");
-  //         done();
-  //       })
-  //       .catch((err) => {
-  //         done(err);
-  //       });
-  //   });
 
-  it("async await based way", async () => {
+  it("done based way", function (done) {
+    axios
+      .get("https://reqres.in/api/users/2")
+      .then((res) => {
+        expect(res.data.data.email).to.be.equal("janet.weaver@reqres.in");
+        done();
+      })
+      .catch((err) => {
+        done(err);
+      });
+  });
+
+  it("async await based way", async function () {
     const res = await axios.get("https://reqres.in/api/users/2");
     expect(res.data.data.email).to.be.equal("janet.weaver@reqres.in");
   });

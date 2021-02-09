4573e5282abd2ea2815a6fd0efb67b6358ff957a
jinho6225 <jinho6225@hotmail.com>
2021-02-08 21:11:46 -0800

    added sinon.js and tested with sinon spy

--- mocha-demo/test/app.spec.js
+++ mocha-demo/test/app.spec.js
@@ -2,28 +2,28 @@ const { add, sub } = require("../src/app");
 const expect = require("chai").expect;
 
 // BDD
-// describe("Suite 1", () => {
-//   afterEach(() => {
-//     console.log("afterEach");
-//   });
-//   beforeEach(() => {
-//     console.log("beforeEach");
-//   });
-//   after(() => {
-//     console.log("after");
-//   });
-//   before(() => {
-//     console.log("before");
-//   });
+describe("Suite 1", () => {
+  afterEach(() => {
+    console.log("afterEach");
+  });
+  beforeEach(() => {
+    console.log("beforeEach");
+  });
+  after(() => {
+    console.log("after");
+  });
+  before(() => {
+    console.log("before");
+  });
 
-//   it("add(2,3) should return 5", () => {
-//     console.log("it");
-//   });
+  it("add(2,3) should return 5", () => {
+    console.log("it");
+  });
 
-//   it("two", () => {
-//     console.log("it two");
-//   });
-// });
+  it("two", () => {
+    console.log("it two");
+  });
+});
 
 /**
  * 
@@ -42,27 +42,27 @@ after
 
 // TDD
 
-suite("Suite 2", () => {
-  suiteSetup(() => {
-    console.log("suiteSetup");
-  });
-  suiteTeardown(() => {
-    console.log("suiteTeardown");
-  });
-  setup(() => {
-    console.log("setup");
-  });
-  teardown(() => {
-    console.log("teardown");
-  });
-  test("add(3,3) should return 6", () => {
-    console.log("test");
-  });
+// suite("Suite 2", () => {
+//   suiteSetup(() => {
+//     console.log("suiteSetup");
+//   });
+//   suiteTeardown(() => {
+//     console.log("suiteTeardown");
+//   });
+//   setup(() => {
+//     console.log("setup");
+//   });
+//   teardown(() => {
+//     console.log("teardown");
+//   });
+//   test("add(3,3) should return 6", () => {
+//     console.log("test");
+//   });
 
-  test("add(1,3) should return 4", () => {
-    console.log("test2");
-  });
-});
+//   test("add(1,3) should return 4", () => {
+//     console.log("test2");
+//   });
+// });
 
 /**
  * 
---
+++ mocha-demo/test/calc.spec.js
@@ -0,0 +1,88 @@
+const { myCalc } = require("../src/calc");
+const expect = require("chai").expect;
+const sinon = require("sinon");
+const chai = require("chai");
+const sinonChai = require("sinon-chai");
+chai.use(sinonChai);
+
+// BDD
+describe("Basic Tests", () => {
+  it("sum - should return correct output with positive values", () => {
+    const result = myCalc.add(2, 3);
+    expect(result).to.equal(5);
+  });
+
+  it("sum - should return correct output with negative values", () => {
+    const result = myCalc.add(-2, -3);
+    expect(result).to.equal(-5);
+  });
+
+  it("mul - should return positive values", () => {
+    const result = myCalc.mul(2, 2);
+    expect(result).to.equal(4);
+  });
+
+  it("div - should return Infinity", () => {
+    const result = myCalc.div(2, 0);
+    expect(result).to.equal(Infinity);
+  });
+});
+
+describe("spy calculator method callback tests", () => {
+  it("myCalc - should call callback once", () => {
+    // Arrange
+    const spy = sinon.spy();
+    // Act
+    myCalc.myCalc(1, 2, spy);
+    sinon.assert.calledOnce(spy); //sinon assertion
+    myCalc.myCalc(1, 2, spy);
+    // Assert
+    sinon.assert.calledTwice(spy); //sinon assertion
+    sinon.assert.calledWithExactly(spy, 1, 2); //sinon assertion
+  });
+});
+
+describe("spy calculator method callback tests", () => {
+  let spy;
+  beforeEach(() => {
+    spy = sinon.spy(myCalc, "sum");
+  });
+  afterEach(() => {
+    spy.restore();
+  });
+  it("myCalc - should call sum once with args 1 and 2", () => {
+    // Arrange
+    // Act
+    myCalc.sum(1, 2);
+    sinon.assert.calledOnce(spy); //sinon assertion
+    // Assert
+    sinon.assert.calledWith(spy, 1, 2);
+  });
+  it("myCalc - should call sum once with args 10 and 20", () => {
+    // Arrange
+    // Act
+    myCalc.sum(10, 20);
+    sinon.assert.calledOnce(spy); //sinon assertion
+    // Assert
+    sinon.assert.calledWith(spy, 10, 20);
+  });
+});
+
+describe.only("spy calculator property getter and setter tests", () => {
+  let spy;
+  beforeEach(() => {
+    spy = sinon.spy(myCalc, "calc", ["get", "set"]);
+  });
+  afterEach(() => {
+    spy.restore();
+  });
+  it("myCalc - getter and setter test", () => {
+    expect(myCalc.calc).to.be.equal("Casio");
+    sinon.assert.calledOnce(spy.get);
+    // Act
+    myCalc.calc = "Genuine";
+    // Assert
+    sinon.assert.calledOnce(spy.set);
+    expect(myCalc.calc).to.be.equal("Genuine");
+  });
+});

c06e4570b3145de691c5de9e087ce51c4e3191a6
jinho6225 <jinho6225@hotmail.com>
2021-02-08 19:49:07 -0800

    async with done argument

--- mocha-demo/test/async.app.spec.js
+++ mocha-demo/test/async.app.spec.js
@@ -3,26 +3,26 @@ const axios = require("axios");
 
 // BDD
 describe("async test suite", () => {
-  it("promised based way", () => {
-    return axios.get("https://reqres.in/api/users/2").then((res) => {
-      expect(res.data.data.email).to.be.equal("janet.weaver@reqres.int");
-    });
-  });
+  //   it("promised based way", () => {
+  //     return axios.get("https://reqres.in/api/users/2").then((res) => {
+  //       expect(res.data.data.email).to.be.equal("janet.weaver@reqres.int");
+  //     });
+  //   });
 
   it("Test 2", () => {
     console.log("test2");
   });
-  //   it("done based way", (done) => {
-  //     axios
-  //       .get("https://reqres.in/api/users/2")
-  //       .then((res) => {
-  //         expect(res.data.data.email).to.be.equal("janet.weaver@reqres.int");
-  //         done();
-  //       })
-  //       .catch((err) => {
-  //         done(err);
-  //       });
-  //   });
+  it("done based way", (done) => {
+    axios
+      .get("https://reqres.in/api/users/2")
+      .then((res) => {
+        expect(res.data.data.email).to.be.equal("janet.weaver@reqres.int");
+        done();
+      })
+      .catch((err) => {
+        done(err);
+      });
+  });
 
   //   it("async await based way", () => {});
 });

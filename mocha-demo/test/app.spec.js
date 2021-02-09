83ecaa8d20b835f63439ed54b019c6070a8a43bb
jinho6225 <jinho6225@hotmail.com>
2021-02-08 19:37:06 -0800

    tdd version hooks

--- mocha-demo/test/app.spec.js
+++ mocha-demo/test/app.spec.js
@@ -2,25 +2,79 @@ const { add, sub } = require("../src/app");
 const expect = require("chai").expect;
 
 // BDD
-describe("Suite 1", () => {
-  afterEach(() => {
-    console.log("afterEach");
+// describe("Suite 1", () => {
+//   afterEach(() => {
+//     console.log("afterEach");
+//   });
+//   beforeEach(() => {
+//     console.log("beforeEach");
+//   });
+//   after(() => {
+//     console.log("after");
+//   });
+//   before(() => {
+//     console.log("before");
+//   });
+
+//   it("add(2,3) should return 5", () => {
+//     console.log("it");
+//   });
+
+//   it("two", () => {
+//     console.log("it two");
+//   });
+// });
+
+/**
+ * 
+Suite 1
+before
+beforeEach
+it
+    ✓ add(2,3) should return 5
+afterEach
+beforeEach
+it two
+    ✓ two
+afterEach
+after
+ */
+
+// TDD
+
+suite("Suite 2", () => {
+  suiteSetup(() => {
+    console.log("suiteSetup");
   });
-  beforeEach(() => {
-    console.log("beforeEach");
+  suiteTeardown(() => {
+    console.log("suiteTeardown");
   });
-  after(() => {
-    console.log("after");
+  setup(() => {
+    console.log("setup");
   });
-  before(() => {
-    console.log("before");
+  teardown(() => {
+    console.log("teardown");
+  });
+  test("add(3,3) should return 6", () => {
+    console.log("test");
   });
 
-  it("add(2,3) should return 5", () => {
-    console.log("it");
-  });
-
-  it("two", () => {
-    console.log("it two");
+  test("add(1,3) should return 4", () => {
+    console.log("test2");
   });
 });
+
+/**
+ * 
+  Suite 2
+suiteSetup
+setup
+test
+    ✓ add(3,3) should return 6
+teardown
+setup
+test2
+    ✓ add(1,3) should return 4
+teardown
+suiteTeardown
+ */

f38bc86b5b9ddf3136a8930ccb69528e02f4e291
jinho6225 <jinho6225@hotmail.com>
2021-02-23 03:57:13 -0800

    functional queue

--- data-structures/sprint-one/src/functional/queue.js
+++ data-structures/sprint-one/src/functional/queue.js
@@ -1,18 +1,26 @@
-var Queue = function() {
+var Queue = function () {
   var someInstance = {};
 
   // Use an object with numeric keys to store values
   var storage = {};
-
+  var start = 0;
+  var end = 0;
   // Implement the methods below
 
-  someInstance.enqueue = function(value) {
+  someInstance.enqueue = function (value) {
+    storage[end++] = value;
   };
 
-  someInstance.dequeue = function() {
+  someInstance.dequeue = function () {
+    if (start < end) {
+      let first = storage[start];
+      start++;
+      return first;
+    }
   };
 
-  someInstance.size = function() {
+  someInstance.size = function () {
+    return end - start;
   };
 
   return someInstance;

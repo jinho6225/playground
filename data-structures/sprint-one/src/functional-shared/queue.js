65a466622e3981622ec13ded322d848f55da6081
jinho6225 <jinho6225@hotmail.com>
2021-02-24 16:28:26 -0800

    functional shared - stack, queue

--- data-structures/sprint-one/src/functional-shared/queue.js
+++ data-structures/sprint-one/src/functional-shared/queue.js
@@ -1,8 +1,32 @@
-var Queue = function() {
+var Queue = function () {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var someInstance = {};
+  someInstance.storage = {};
+  someInstance.start = 0;
+  someInstance.end = 0;
+  extend(someInstance, queueMethods);
+  return someInstance;
 };
 
-var queueMethods = {};
+var extend = function (obj1, obj2) {
+  for (let key in obj2) {
+    obj1[key] = obj2[key];
+  }
+};
 
-
+var queueMethods = {
+  enqueue(value) {
+    this.storage[this.end++] = value;
+  },
+  dequeue() {
+    if (this.start < this.end) {
+      let first = this.storage[this.start];
+      this.start++;
+      return first;
+    }
+  },
+  size() {
+    return this.end - this.start;
+  },
+};
--- data-structures/sprint-one/src/functional-shared/stack.js
+++ data-structures/sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,32 @@
-var Stack = function() {
+var Stack = function () {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var someInstance = {};
+  someInstance.storage = {};
+  someInstance.count = 0;
+  extend(someInstance, stackMethods);
+  return someInstance;
 };
 
-var stackMethods = {};
+var extend = function (obj1, obj2) {
+  for (let key in obj2) {
+    obj1[key] = obj2[key];
+  }
+};
 
-
+var stackMethods = {
+  push: function (value) {
+    this.storage[++this.count] = value;
+  },
+  pop: function () {
+    if (this.count > 0) {
+      let lastEl = this.storage[this.count];
+      delete this.storage[this.count];
+      this.count--;
+      return lastEl;
+    }
+  },
+  size: function () {
+    return this.count;
+  },
+};

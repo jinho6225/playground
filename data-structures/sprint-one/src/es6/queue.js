6688fde2e0bf2e046d8c2e844ca2e285de86dd45
jinho6225 <jinho6225@hotmail.com>
2021-02-26 00:44:41 -0800

    es6 class stack, queue

--- data-structures/sprint-one/src/es6/queue.js
+++ data-structures/sprint-one/src/es6/queue.js
@@ -2,6 +2,24 @@ class Queue {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this.storage = {};
+    this.start = 0;
+    this.end = 0;
   }
 
+  enqueue(value) {
+    this.storage[this.end++] = value;
+  }
+
+  dequeue() {
+    if (this.start < this.end) {
+      let first = this.storage[this.start];
+      this.start++;
+      return first;
+    }
+  }
+
+  size() {
+    return this.end - this.start;
+  }
 }
--- data-structures/sprint-one/src/es6/stack.js
+++ data-structures/sprint-one/src/es6/stack.js
@@ -2,6 +2,24 @@ class Stack {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this.storage = {};
+    this.count = 0;
   }
 
+  push(value) {
+    this.storage[++this.count] = value;
+  }
+
+  pop() {
+    if (this.count > 0) {
+      let lastEl = this.storage[this.count];
+      delete this.storage[this.count];
+      this.count--;
+      return lastEl;
+    }
+  }
+
+  size() {
+    return this.count;
+  }
 }

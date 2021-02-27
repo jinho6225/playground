294b171ff22543eed4acb4c456560848d293e59e
jinho6225 <jinho6225@hotmail.com>
2021-02-27 01:34:52 -0800

    es6 class style process

--- 6ees6ees6ees/src/Bee.js
+++ 6ees6ees6ees/src/Bee.js
@@ -1,3 +1,9 @@
-class Bee {
+class Bee extends Grub {
   // TODO..
-};
+  constructor(food, eat) {
+    super(food, eat);
+    this.age = 5;
+    this.color = "yellow";
+    this.job = "Keep on growing";
+  }
+}
--- 6ees6ees6ees/src/HoneyMakerBee.js
+++ 6ees6ees6ees/src/HoneyMakerBee.js
@@ -1,3 +1,15 @@
-class HoneyMakerBee {
+class HoneyMakerBee extends Bee {
   // TODO..
-};
+  constructor(food, eat, color) {
+    super(food, eat, color);
+    this.age = 10;
+    this.job = "make honey";
+    this.honeyPot = 0;
+  }
+  makeHoney() {
+    this.honeyPot++;
+  }
+  giveHoney() {
+    this.honeyPot--;
+  }
+}

8e79fbcdeb37efd2fa6afacf8cfabfbf5f7cb4a9
jinho6225 <jinho6225@hotmail.com>
2021-02-28 03:16:26 -0800

    linked list

--- data-structures/sprint-two/src/linkedList.js
+++ data-structures/sprint-two/src/linkedList.js
@@ -1,21 +1,38 @@
-var LinkedList = function() {
+var LinkedList = function () {
   var list = {};
   list.head = null;
   list.tail = null;
-
-  list.addToTail = function(value) {
+  //{} - {} - {}
+  list.addToTail = function (value) {
+    let node = Node(value);
+    if (!this.head && !this.tail) {
+      this.head = node;
+      this.tail = node;
+    } else {
+      this.tail.next = node;
+      this.tail = node;
+    }
   };
 
-  list.removeHead = function() {
+  list.removeHead = function () {
+    let head = this.head;
+    this.head = this.head.next;
+    return head.value;
   };
 
-  list.contains = function(target) {
+  list.contains = function (target) {
+    let cur = this.head;
+    while (cur) {
+      if (target === cur.value) return true;
+      cur = cur.next;
+    }
+    return false;
   };
 
   return list;
 };
 
-var Node = function(value) {
+var Node = function (value) {
   var node = {};
 
   node.value = value;

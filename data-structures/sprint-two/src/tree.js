1bce1334ccdb7df0fb72209d27170ac1bdb0ca51
jinho6225 <jinho6225@hotmail.com>
2021-02-28 12:38:27 -0800

    tree data structure

--- data-structures/sprint-two/src/tree.js
+++ data-structures/sprint-two/src/tree.js
@@ -1,22 +1,41 @@
-var Tree = function(value) {
+var Tree = function (value) {
   var newTree = {};
   newTree.value = value;
 
   // your code here
-  newTree.children = null;  // fix me
-
+  newTree.children = []; // fix me
+  extend(newTree, treeMethods);
   return newTree;
 };
 
+function extend(obj1, obj2) {
+  for (let key in obj2) {
+    obj1[key] = obj2[key];
+  }
+}
+
 var treeMethods = {};
 
-treeMethods.addChild = function(value) {
+treeMethods.addChild = function (value) {
+  let node = Tree(value);
+  this.children.push(node);
 };
 
-treeMethods.contains = function(target) {
+treeMethods.contains = function (target) {
+  let flag = false;
+  function helper(obj) {
+    if (obj.children.length === 0) return;
+    for (let i = 0; i < obj.children.length; i++) {
+      if (obj.children[i].value === target) {
+        flag = true;
+        return;
+      }
+      helper(obj.children[i]);
+    }
+  }
+  helper(this);
+  return flag;
 };
-
-
 
 /*
  * Complexity: What is the time complexity of the above functions?

7429e5c1a6d28e82669d55707fb3695bcd88b757
jinho6225 <jinho6225@hotmail.com>
2021-03-10 21:40:14 -0800

    composition vs inheritance

---
+++ advanced_javascript/composition_vs_inheritance.js
@@ -0,0 +1,17 @@
+//extends <- inheritance
+
+// composition vs inheritance
+
+
+//Inheritance
+//what it is
+
+//tight coupling <=> reusable
+//Fragile Base Class Problem
+//Hierarchy
+ 
+
+
+
+//Composition
+//what it has, what it does

946709132e42b859c88425995c15e960bbc45078
jinho6225 <jinho6225@hotmail.com>
2021-03-30 21:32:49 -0700

    functional

--- data-structures/sprint-one/src/functional/queue.js
+++ data-structures/sprint-one/src/functional/queue.js
@@ -3,16 +3,25 @@ var Queue = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
+  let count = 0
+  let deCount = 0
 
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    storage[++count] = value
   };
 
   someInstance.dequeue = function() {
+    if (count > deCount) {
+      deCount++
+    }
+    let result = storage[deCount]
+    return result
   };
 
   someInstance.size = function() {
+    return count - deCount
   };
 
   return someInstance;
--- data-structures/sprint-one/src/functional/stack.js
+++ data-structures/sprint-one/src/functional/stack.js
@@ -3,15 +3,24 @@ var Stack = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
+  let count = 0
 
   // Implement the methods below
   someInstance.push = function(value) {
+    storage[++count] = value
   };
 
   someInstance.pop = function() {
+    let result = storage[count]
+    delete storage[count]
+    if (count > 0) {
+      count--
+    }
+    return result
   };
 
   someInstance.size = function() {
+    return count
   };
 
   return someInstance;

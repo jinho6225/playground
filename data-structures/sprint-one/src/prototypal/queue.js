d466ce0d0a733176d21fac216d9f5be8d3956615
jinho6225 <jinho6225@hotmail.com>
2021-04-01 12:24:00 -0700

    prototypal, Object.create()

--- data-structures/sprint-one/src/prototypal/queue.js
+++ data-structures/sprint-one/src/prototypal/queue.js
@@ -1,8 +1,28 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var queue = Object.create(queueMethods);
+  queue.count = 0;
+  queue.deCount = 0;
+  queue.storage = {}
+  return queue;
 };
 
 var queueMethods = {};
 
+queueMethods.enqueue = function(value) {
+this.storage[++this.count] = value
+};
+
+queueMethods.dequeue = function() {
+if (this.count > this.deCount) {
+  this.deCount++
+}
+let result = this.storage[this.deCount]
+return result
+};
+
+queueMethods.size = function() {
+return this.count - this.deCount
+};
 
--- data-structures/sprint-one/src/prototypal/stack.js
+++ data-structures/sprint-one/src/prototypal/stack.js
@@ -1,8 +1,25 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var stack = Object.create(stackMethods);
+  stack.count = 0;
+  stack.storage = {};
+  return stack;
 };
 
 var stackMethods = {};
 
-
+stackMethods.size = function() {
+  return this.count
+};
+stackMethods.push = function(val) {
+  this.storage[++this.count] = val
+};
+stackMethods.pop = function() {
+  let result = this.storage[this.count]
+  delete this.storage[this.count]
+  if (this.count > 0) {
+    this.count--
+  }
+  return result
+};

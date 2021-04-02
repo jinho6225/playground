9c3741c73e9562022bdcab6e9c66a0b912662807
jinho6225 <jinho6225@hotmail.com>
2021-04-02 15:00:20 -0700

    es6 class

--- data-structures/sprint-one/src/es6/queue.js
+++ data-structures/sprint-one/src/es6/queue.js
@@ -2,6 +2,25 @@ class Queue {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this.count = 0;
+    this.deCount = 0
+    this.storage = {};
   }
+
+  size() {
+    return this.count - this.deCount
+  };
+
+  enqueue(val) {
+    this.storage[++this.count] = val
+  };
+
+  dequeue() {
+    if (this.count > this.deCount) {
+      this.deCount++
+    }
+    let result = this.storage[this.deCount]
+    return result
+  };
 
 }

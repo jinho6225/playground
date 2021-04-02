9b7bc42fcf1db054befb405b9f5a2a41361bdfde
jinho6225 <jinho6225@hotmail.com>
2021-04-01 20:40:40 -0700

    pseudoclassical new keyword

--- data-structures/sprint-one/src/pseudoclassical/queue.js
+++ data-structures/sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,28 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.count = 0;
+  this.deCount = 0
+  this.storage = {};
 };
+
+Queue.prototype.size = function() {
+    return this.count - this.deCount
+};
+
+Queue.prototype.enqueue = function(val) {
+  this.storage[++this.count] = val
+};
+
+Queue.prototype.dequeue = function() {
+  if (this.count > this.deCount) {
+    this.deCount++
+  }
+  let result = this.storage[this.deCount]
+  return result
+};
+
+
+var queue = new Queue();
 
 
--- data-structures/sprint-one/src/pseudoclassical/stack.js
+++ data-structures/sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,27 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.count = 0;
+  this.storage = {};
+  
+};
+
+Stack.prototype.size = function() {
+    return this.count  
+};
+
+Stack.prototype.push = function(val) {
+  this.storage[++this.count] = val
+};
+
+Stack.prototype.pop = function() {
+  let result = this.storage[this.count]
+  delete this.storage[this.count]
+  if (this.count > 0) {
+    this.count--
+  }
+  return result
 };
 
 
+var stack = new Stack();

6742dc7f135bc4d3a87ce2ba716c767bb400dd26
jinho6225 <jinho6225@hotmail.com>
2021-04-18 22:12:25 -0700

    added merge sort, quick sort

--- data-structures/sorting/mergeSort.js
+++ data-structures/sorting/mergeSort.js
@@ -1,4 +1,4 @@
-const numbers = [99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0];
+let numbers = [99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 60];
 
 function mergeSort (array) {
   if (array.length === 1) {
@@ -8,33 +8,32 @@ function mergeSort (array) {
   let mid = Math.floor(array.length / 2)
   let left = array.slice(0, mid)
   let right = array.slice(mid)
+
   return merge(
     mergeSort(left),
     mergeSort(right)
   )
 }
 
-function merge(left, right) {
-	let result = []
+function merge(left, right){
+	let resultArr = []
 	let l = 0, r = 0
-	while (left.length > l || right.length > r) {
-		if (left[l] && right[r] === undefined) {
-			result.push(left[l])
+	while (l < left.length || r < right.length) {
+		if (left[l] && !right[r]) {
+			resultArr.push(left[l])
 			l++
-		} else if (left[l] === undefined && right[r]) {
-			result.push(right[r])
+		} else if (!left[l] && right[r]) {
+			resultArr.push(right[r])
 			r++
-		} else {
-			if (left[l] > right[r]) {
-				result.push(right[r])
-				r++
-			} else {
-				result.push(left[l])
-				l++
-			}
-		}
+		} else if (left[l] >= right[r]) {
+			resultArr.push(right[r])
+			r++
+		} else if (left[l] < right[r]) {
+			resultArr.push(left[l])
+			l++
+		}		
 	}
-	return result
+	return resultArr
 }
 
 

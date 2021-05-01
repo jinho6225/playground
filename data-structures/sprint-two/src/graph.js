1de0ef421186fb1cdf7ae66cd805493ff7c5bfcf
jinho6225 <jinho6225@hotmail.com>
2021-05-01 11:18:49 -0700

    added kind of graph explanation

--- data-structures/sprint-two/src/graph.js
+++ data-structures/sprint-two/src/graph.js
@@ -1,8 +1,26 @@
+//Edge List
+const graph = [[0,2], [2,3], [2,1], [1,3]]
+//Adjacent List
+const graph2 = [[2], [2,3], [0,1,3], [1,2]]
+//Adjacent Matrix
+const graph3 = {
+    0:[0,0,1,0],
+    1:[0,0,1,1],
+    2:[1,1,0,1],
+    3:[0,1,1,0]
+}
+/**
+    2   -   0
+  /   \ 
+1  -   3
+
+ */
+
 
 
 // Instantiate a new graph
 var Graph = function() {
-    this.storage = {}
+    this.node = {}
 };
 
 // Add a node to the graph, passing in the node's value.

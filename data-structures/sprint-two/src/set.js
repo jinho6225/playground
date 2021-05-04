5baa046c57c8629afb8eca64d2eff84490c28210
jinho6225 <jinho6225@hotmail.com>
2021-05-04 00:42:56 -0700

    set

--- data-structures/sprint-two/src/set.js
+++ data-structures/sprint-two/src/set.js
@@ -1,18 +1,31 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = null; // fix me
+  set._storage = {}; // fix me
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item) {
+  console.log(this, 'this', item)
+  this._storage[item] = true
 };
 
 setPrototype.contains = function(item) {
+  for (let key in this._storage) {
+    if (item === key) {
+      return true
+    }
+  }
+  return false
 };
 
 setPrototype.remove = function(item) {
+  for (let key in this._storage) {
+    if (item === key) {
+      delete this._storage[item]
+    }
+  }  
 };
 
 /*
